b9cfcfebac4f8337c888adafc08383052d790dfd
Robert D Gonzales <robert.gonzo13@yahoo.com>
2022-07-11 18:44:30 -0500

    added controller methods to routes for both users and thoughts

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,29 +1,36 @@
-const { get } = require('mongoose');
-
 const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
 
 // /api/thoughts
 router
 .route('/')
-.get()
-.post();
+.get(getAllThoughts)
+.post(createThought);
 
 // /api/thoughts/:thoughtId
 router
 .route('/:thoughtId')
-.get()
-.put()
-.delete();
+.get(getThoughtById)
+.put(updateThought)
+.delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
 router
 .route('/:thoughtId/reactions')
-.post()
+.post(createReaction)
 
 // /api/thoughts/:thoughtId/reactions/:reactionId
 router
 .route('/:thoughtId/reactions/:reactionId')
-.delete();
+.delete(deleteReaction);
 
 
 module.exports = router;
